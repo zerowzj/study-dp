4f4189becbb36f6086e2484e9c3264c8c6ab5da2
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-06-17 14:12:43 +0800

    更新代码

---
+++ src/main/java/study/dp/command/NoCommand.java
@@ -0,0 +1,14 @@
+package study.dp.command;
+
+public class NoCommand implements Command {
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void undo() {
+
+    }
+}
