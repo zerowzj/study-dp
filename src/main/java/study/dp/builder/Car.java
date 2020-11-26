b5b6f7bad679841cbd66467416c2821a350a53f7
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-11-26 10:58:52 +0800

    更新代码

--- src/main/java/study/dp/builder/Car.java
+++ src/main/java/study/dp/builder/Car.java
@@ -1,12 +1,59 @@
 package study.dp.builder;
 
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
 public class Car {
 
     private String brand;
 
     private String logo;
 
-    class Builder {
+    /**
+     * 1.私有
+     * 2.入参 Builder
+     */
+    private Car(Builder builder) {
+        this.brand = builder.brand;
+        this.logo = builder.logo;
+    }
 
+    /**
+     * 1.构造器类
+     */
+    static class Builder {
+
+        /**
+         * 1.可以提供默认值
+         */
+        private String brand = "audi";
+
+        private String logo = "audi";
+
+        /**
+         * 1.根据参数的必填 或 可选设计构造函数
+         */
+        public Builder() {
+        }
+
+        public Builder(String brand) {
+            this.brand = brand;
+        }
+
+        private Builder brand(String brand) {
+            this.brand = brand;
+            return this;
+        }
+
+        public Car build() {
+            return new Car(this);
+        }
+    }
+
+    public static void main(String[] args) {
+        Car car = new Car.Builder().build();
+       log.info(">>>>>> {}", car.getBrand());
     }
 }
